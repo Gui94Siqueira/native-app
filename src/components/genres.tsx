ca831593c9ad1a6bc6b8643f81574979c60b9bbb
Guilherme <guilherme94siqueira@gmail.com>
2024-10-24 21:38:14 -0300

    update

--- src/components/genres.tsx
+++ src/components/genres.tsx
@@ -1,9 +1,11 @@
 import { Text, View } from "react-native";
 
+
 export function Genres() {
     return(
         <View className="ml-4 mt-6">
             <Text className="font-bold text-sm">Procure pelos generos</Text>
+        
         </View>
     )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,13 @@
+import {colors} from "./src/styles/colors"
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './App.{js,jsx,ts,tsx}',
-    './components/**/*.{js,jsx,ts,tsx}',
-  ],
+  
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  presets: [require("nativewind/preset")],
   theme: {
-    extend: {},
+    extend: {
+      colors,
+    },
   },
   plugins: [],
-};
+}
